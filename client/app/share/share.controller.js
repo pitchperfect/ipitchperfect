4b6c2f6a19ac3ba7c98415f53dfe9dfc724c5219
Christopher Gray <areagray@gmail.com>
2014-08-02 16:42:09 -0700

    feat/share added UI elements

--- client/app/share/share.controller.js
+++ client/app/share/share.controller.js
@@ -3,4 +3,9 @@
 angular.module('pitchPerfectApp')
   .controller('ShareCtrl', function ($scope) {
     $scope.message = 'Hello';
+    $scope.showNote = false;
+    $scope.toggleNote = function(){
+    	$scope.showNote = !$scope.showNote;
+    };
+
   });
