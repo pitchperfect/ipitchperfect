4bdcadf4fe053fdc9914a5543eebe83885295007
Christopher Gray <areagray@gmail.com>
2014-08-02 17:07:54 -0700

    feat/share add confirm modal partial, service

--- client/app/share/share.controller.js
+++ client/app/share/share.controller.js
@@ -1,11 +1,27 @@
 'use strict';
 
 angular.module('pitchPerfectApp')
-  .controller('ShareCtrl', function ($scope) {
+  .controller('ShareCtrl', function ($scope, $location, modalService) {
     $scope.message = 'Hello';
     $scope.showNote = false;
     $scope.toggleNote = function(){
     	$scope.showNote = !$scope.showNote;
     };
+
+	var modalOptions = {
+		//closeButtonText: 'Done',
+		//actionButtonText: 'Done',
+		headerText: 'You done shared...',
+		bodyText: 'You\ve just shared your interview with Alan. Get ready to get some awesome feedback that will change your life'
+	};
+
+	$scope.showModal = function(){
+	modalService.showModal({}, modalOptions).then(function(result) {
+		$location.path('/home');
+	});
+
+	};
+
+
 
   });
