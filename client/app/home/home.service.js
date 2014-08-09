a1214d3547871ba62d29f87ce04fb0b81576d5cd
celineotter <celine.otter@gmail.com>
2014-08-09 09:43:58 -0700

    (fix) Previous commit broke loading through dist

--- client/app/home/home.service.js
+++ client/app/home/home.service.js
@@ -6,51 +6,16 @@ angular.module('pitchPerfectApp')
 .factory('HomeFactory', function($http) {
 
 
-  // var createDeck = function (postDeckObject, callback) {
-  //   $http.post('/api/decks', postDeckObject)
-  //   .success(function() {
-  //     callback();
-  //   });
-  // };
-  //
-  //
-  // return {
-  //   createDeck: createDeck,
-  // };
-
-
-  var getAllUserDecks = function (getUserDecksCb) {
-    $http.get('/api/userdecks').success(function(allUserDecks) {
-      console.log('@home received userdecks', allUserDecks);
-
-      getUserDecksCb(allUserDecks);
-
-    });
-  };
-
-  var getAllDecks = function (getDecksCb, getUserDecksCb) {
-    $http.get('/api/decks').success(function(allDecks) {
-      console.log('@home received decks', allDecks);
-
-      getDecksCb(allDecks);
-      this.getAllUserDecks(getUserDecksCb);
-    });
-  };
-  // HomeFactory.createDeck(postDeckObject, $scope.createDeckCb, $scope.getDecksCb, $scope.getUserDecksCb);
-  var createDeck = function (postDeckObject, createDeckCb, getDecksCb, getUserDecksCb) {
+  var createDeck = function (postDeckObject, callback) {
     $http.post('/api/decks', postDeckObject)
     .success(function() {
-      postDeckObject();
-      this.getAllDecks(getDecksCb, getUserDecksCb);
+      callback();
     });
   };
 
 
   return {
     createDeck: createDeck,
-    getAllDecks: getAllDecks,
-    getAllUserDecks: getAllUserDecks,
   };
-
 
 });
