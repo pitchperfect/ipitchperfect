c5bab2f3d7193d78c2a15b9a28c27b9d663a6df3
David Deriso <davidderiso@gmail.com>
2014-08-11 11:18:43 -0700

    (fix) Fixed broken karma tests. They now work when typing 'grunt' from command line

--- client/app/interview/interview.controller.spec.js
+++ client/app/interview/interview.controller.spec.js
@@ -5,21 +5,21 @@ describe('Controller: InterviewCtrl', function () {
   // load the controller's module
   beforeEach(module('pitchPerfectApp'));
 
-  // var InterviewCtrl, scope;
-  //
-  // // Initialize the controller and a mock scope
-  // beforeEach(inject(function ($controller, $rootScope) {
-  //   scope = $rootScope.$new();
-  //   var win = window.open();
-  //
-  //   InterviewCtrl = $controller('InterviewCtrl', {
-  //     $scope: scope,
-  //     $window: win,
-  //     $interval: win.interval
-  //   });
-  // }));
-  //
-  // it('should ...', function () {
-  //   expect(1).toEqual(1);
-  // });
+  var InterviewCtrl, scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    var win = window.open();
+
+    InterviewCtrl = $controller('InterviewCtrl', {
+      $scope: scope,
+      $window: win,
+      $interval: win.interval
+    });
+  }));
+
+  it('should ...', function () {
+    expect(1).toEqual(1);
+  });
 });
