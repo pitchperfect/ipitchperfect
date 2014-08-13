b245543771d79775a0b5041bd8a2bbb9d48b682f
Christopher Gray <areagray@gmail.com>
2014-08-12 19:56:15 -0700

    feature/share Add modal, parse and process invitee list, ready for emailer

---
+++ client/app/share/share.service.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('pitchPerfectApp')
+
+
+.factory('ShareFactory', function($http) {
+
+
+  var getAllUsers = function (callback) {
+    $http.get('/api/users')
+    .success(function(allUsers) {
+      console.log(allUsers);
+      callback(allUsers);
+    }).error(function(data, status, headers, config) {
+      console.log('getAllUsers error:', data, status, headers, config);
+    });
+  };
+
+
+  var sendInvites = function(invites){
+
+    // For Celine
+    // invites contains the user names and emails for the invites
+    // must be validated
+
+    console.log('invites to be handled and validated on server', invites);
+  };
+
+  return {
+    getAllUsers: getAllUsers,
+    sendInvites: sendInvites
+  };
+
+});
