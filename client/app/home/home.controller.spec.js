5c71db89656c4f45c4fe241b116e4eba6c7c73e2
David Deriso <davidderiso@gmail.com>
2014-08-12 20:14:36 -0700

    Additional development for automated test cases

--- client/app/home/home.controller.spec.js
+++ client/app/home/home.controller.spec.js
@@ -1,21 +1,37 @@
 'use strict';
 
 describe('Controller: HomeCtrl', function () {
-  //
-  // // load the controller's module
-  // beforeEach(module('pitchPerfectApp'));
-  //
-  // var HomeCtrl, scope;
-  //
-  // // Initialize the controller and a mock scope
-  // beforeEach(inject(function ($controller, $rootScope) {
-  //   scope = $rootScope.$new();
-  //   HomeCtrl = $controller('HomeCtrl', {
-  //     $scope: scope
-  //   });
-  // }));
-  //
-  it('should ...', function () {
-    expect(1).toEqual(1);
+  // load the controller's module
+  var HomeCtrl, $scope, $http, HomeFactory, $resource, InterviewFactory, $state;
+
+  beforeEach(module('pitchPerfectApp'));
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope, _$http_, _HomeFactory_, _$resource_, _InterviewFactory_, _$state_) {
+    $scope = $rootScope.$new();
+    $http = _$http_;
+    HomeFactory = _HomeFactory_;
+    $resource = _$resource_;
+    InterviewFactory = _InterviewFactory_;
+    $state = _$state_;
+
+    HomeCtrl = $controller('HomeCtrl', {
+      $scope: $scope,
+      $http: $http,
+      HomeFactory: HomeFactory,
+      $resource: $resource,
+      InterviewFactory: InterviewFactory,
+      $state: $state
+    });
+  }));
+
+
+  it('should properly toggle submit box setting', function() {
+    // toggleSubmitBoxAppear
+    $scope.toggleSubmitBoxAppear();
+    var firstToggle = $scope.submitBox;
+    $scope.toggleSubmitBoxAppear();
+    expect(firstToggle).toEqual(!$scope.submitBox);
   });
+
 });
